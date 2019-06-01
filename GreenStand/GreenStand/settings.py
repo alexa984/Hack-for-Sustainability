615c398fcaca6fde7a783f4bb8ace6ff0b08d1de
alexa984 <alex.yovkova@gmail.com>
2019-06-01 17:17:25 +0300

    added initial views

--- GreenStand/GreenStand/settings.py
+++ GreenStand/GreenStand/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
