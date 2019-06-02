c57ba644b37e944052a65238040fe6b6fff1bfba
elenatuparova <elena.tuparova@gmail.com>
2019-06-02 12:09:08 +0300

    Add url

--- GreenStand/Shop/urls.py
+++ GreenStand/Shop/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.views import View
 
 urlpatterns = [
-path('', views.home, name='home'),
-path('register/', views.register, name='register')
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login')
 ]
