5ef73851bf5853199a881a13ff5b57609e5feb42
elenatuparova <elena.tuparova@gmail.com>
2019-06-02 14:25:46 +0300

    Make registration form

--- GreenStand/Shop/forms.py
+++ GreenStand/Shop/forms.py
@@ -1,15 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from . import models 
 
-class SignUpForm(UserCreationForm):
+class CustomerRegisterForm(ModelForm):
+    username = forms.CharField(max_length=50, required=True, help_text='*')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(max_length=20, required=True, help_text='*')
     last_name = forms.CharField(max_length=20, required=True, help_text='*')
-    email = forms.EmailField(max_length=50, required=True, help_text='*')
+    email = forms.EmailField(max_length=50, required=True, help_text='*',
+        widget=forms.EmailInput(attrs={'class':'form-control'}))
     phone = forms.CharField(max_length=15, required=True, help_text='*')
     address = forms.CharField(max_length=100, required=True, help_text='*')
 
     class Meta:
+        model = models.Customer
+        fields = ('username',  'password1', 'password2', 'first_name', 'last_name', 'email', 'phone', 'address')
+
+class LoginForm(AuthenticationForm):
+
+    class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'phone', 'address', 'password1', 'password2', )
+        fields = ('username', 'password')
--- GreenStand/Shop/views.py
+++ GreenStand/Shop/views.py
@@ -11,22 +11,72 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from . import models
-from .forms import SignUpForm
+from .forms import CustomerRegisterForm, LoginForm
 
 
 def home(request):
     return render(request, 'home.html')
 
-def register(request):
+def customer_register(request):
+    template = 'register.html'
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
+        form = CustomerRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+            if models.Customer.objects.filter(username=form.cleaned_data['username']).exists():
+                return render(request, template, {
+                    'form': form,
+                    'error_message': 'Username already exists.'
+                })
+            elif models.Customer.objects.filter(email=form.cleaned_data['email']).exists():
+                return render(request, template, {
+                    'form': form,
+                    'error_message': 'Email already exists.'
+                })
+            elif form.cleaned_data['password1'] != form.cleaned_data['password2']:
+                return render(request, template, {
+                    'form': form,
+                    'error_message': 'Passwords do not match.'
+                })
+            else:
+                # Create the user:
+                customer = models.Customer.objects.create(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password1'],
+                    first_name=form.cleaned_data['first_name'],
+                    last_name=form.cleaned_data['last_name'],
+                    address=form.cleaned_data['address'],
+                    phone=form.cleaned_data['phone'],
+                    email=form.cleaned_data['email'],
+                )
+                # user.first_name = form.cleaned_data['first_name']
+                # user.last_name = form.cleaned_data['last_name']
+                # user.phone_number = form.cleaned_data['phone_number']
+                # user.save()
+               
+                # Login the user
+                # login(request, customer)
+               
+                # redirect to accounts page:
+                return redirect('home')
+            # form.save()
+            # username = form.cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password')
+            # user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            # return redirect('home')
     else:
-        form = SignUpForm()
+        form = CustomerRegisterForm()
     return render(request, 'register.html', {'form': form})
+
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'login.html', {'form': form})
