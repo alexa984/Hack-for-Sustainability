792a389cdf2186f35431dad00a54228a84cbb2f9
elenatuparova <elena.tuparova@gmail.com>
2019-06-03 17:21:52 +0300

    Add Orders app to settings

--- GreenStand/GreenStand/settings.py
+++ GreenStand/GreenStand/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Shop.apps.ShopConfig',
+    'Orders.apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
